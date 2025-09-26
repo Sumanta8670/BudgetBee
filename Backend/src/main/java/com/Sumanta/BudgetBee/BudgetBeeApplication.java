7285b843a2e2b8ac196f950931319c256d0ab830
Sumanta Jana <janasumanta59@gmail.com>
2025-09-27 00:32:00 +0530

    Project Restructure

---
+++ Backend/src/main/java/com/Sumanta/BudgetBee/BudgetBeeApplication.java
@@ -0,0 +1,13 @@
+package com.Sumanta.BudgetBee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBeeApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/Sumanta/BudgetBee/BudgetBeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sumanta.BudgetBee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
